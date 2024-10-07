2ec83ba3549acc6a9656d7852223e1d760ccec1c
binaryboom <raghavkatariaaa@gmail.com>
2024-10-07 18:02:16 +0530

    2nd

--- index.js
+++ index.js
@@ -2,37 +2,41 @@ const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
 (async () => {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath,
-    headless: true,
-  });
-  const page = await browser.newPage();
-
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
-
+  let browser = null;
   try {
+    browser = await puppeteer.launch({
+      args: [...chromium.args, '--no-sandbox', '--disable-setuid-sandbox'],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless, // Use true for headless mode
+    });
+    
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    
     await page.goto('https://www.coke2home.com/thunderwheels', {
       waitUntil: 'networkidle2',
-      timeout: 60000, // Increase timeout
+      timeout: 60000,
     });
 
     const rewardsValue = await page.evaluate(() => {
-      const labels = ['bike', 'fancode', 'fk gc']; // Add more labels as necessary
+      const labels = ['bike', 'fancode', 'fk gc'];
       const elements = document.querySelectorAll('.voucher-counts .group.flex .data .text');
       const rewards = {};
-  
+
       elements.forEach((el, index) => {
         rewards[labels[index]] = el ? el.innerText : null;
       });
-  
+
       return rewards;
     });
 
     console.log('Rewards won so far:', rewardsValue);
   } catch (error) {
     console.error('Error occurred:', error);
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
-
-  await browser.close();
 })();
